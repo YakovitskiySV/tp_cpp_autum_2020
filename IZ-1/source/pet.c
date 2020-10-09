499eed36a26154399803298899ba41dfaa5d1983
YakovitskiySV <stas_naruto@mail.ru>
2020-10-09 20:04:08 +0300

    Была реализована стрктура и программа, способная на основе этой структуры считывать параметры питомцев и выводить питомцев, группируя их

---
+++ IZ-1/source/pet.c
@@ -0,0 +1,93 @@
+#include "pet.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+
+int CreatePet(Pet *newPet, const char **params) {
+    if (!params) {
+        perror("pointer error");
+    }
+
+    // reading pet's name
+    if (!(params[0])) {
+        perror("pointer error");
+        return 0;
+    }
+    size_t bufSize = strlen(params[0]) + 1;
+    if (!(newPet->name = (char*) malloc(bufSize * sizeof(char)))) {
+        perror("allocation error");
+        return 0;
+    }
+    strncpy(newPet->name, params[0], bufSize);
+
+    // reading pet's type
+    if (!(params[1])) {
+        perror("pointer error");
+        free(newPet->name);
+        return 0;
+    }
+    bufSize = strlen(params[1]) + 1;
+    if (!(newPet->type = (char*) malloc(bufSize * sizeof(char)))) {
+        perror("allocation error");
+        free(newPet->name);
+        return 0;
+    }
+    strncpy(newPet->type, params[1], bufSize);
+
+    // reading pet's color
+    if (!(params[2])) {
+        perror("pointer error");
+        free(newPet->name);
+        free(newPet->color);
+    }
+    bufSize = strlen(params[2]) + 1;
+    if (!(newPet->color = (char*) malloc(bufSize * sizeof(char)))) {
+        perror("allocation error");
+        free(newPet->name);
+        free(newPet->type);
+        return 0;
+    }
+    strncpy(newPet->color, params[2], bufSize);
+    return 1;
+}
+
+int SortPets(Pet *pets, const size_t petsNumber) {
+    if (!pets) {
+        perror("Pointer error");
+        return 0;
+    }
+    for (size_t i = 0 ; i < petsNumber ; i++) {
+        for (size_t j = 0; j < petsNumber - 1; j++) {
+            if (strcmp(pets[j].type, pets[j + 1].type) > 0) {
+                Pet temp = pets[j];
+                pets[j] = pets[j + 1];
+                pets[j + 1] = temp;
+            }
+        }
+    }
+    return 1;
+}
+
+int DeletePet(Pet *pet) {
+    free(pet->name);
+    free(pet->color);
+    free(pet->type);
+    return 1;
+}
+
+int PrintPet(const Pet *pet) {
+    if ((pet->name != NULL)
+       && (pet->color != NULL)
+       && (pet->type != NULL)) {
+        printf("name: %s, type: %s, color: %s\n",
+               pet->name,
+               pet->type,
+               pet->color);
+        return 1;
+    }
+    perror("pointer error");
+    free(pet->name);
+    free(pet->type);
+    free(pet->color);
+    return 0;
+}

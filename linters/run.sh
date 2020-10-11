af3dda9585ed583f1caeb13113a1b1037d02a35c
YakovitskiySV <stas_naruto@mail.ru>
2020-10-12 01:14:28 +0300

    проверка того что проблема в .gitignore

---
+++ linters/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+
+function print_header() {
+    echo -e "\n***** ${1} *****"
+}
+
+cflags="--suppressions-list=.suppressions-list.txt --std=c99 --enable=all -inconclusive"
+cppflags="--suppressions-list=.suppressions-list.txt --std=c++11 --enable=all -inconclusive"
+
+print_header "RUN CPP CHECK"
+
+cppcheck $cflags source/*.c
+cppcheck $cflags includes/*.h
+cppcheck $cppflags tests/*.cpp
+
+
+print_header "RUN cpplint.py"
+python2.7 ./linters/cpplint/cpplint.py --extensions=c source/*
+python2.7 ./linters/cpplint/cpplint.py --extensions=cpp tests/*
+python2.7 ./linters/cpplint/cpplint.py --extensions=h includes/*
+
+
+
+print_header "SUCCESS"

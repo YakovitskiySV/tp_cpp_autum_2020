2312f87de01f5cd57d0d75451cb8ad67e425ff66
YakovitskiySV <stas_naruto@mail.ru>
2020-10-14 05:37:24 +0300

    Учтены замечания по коду 12 linters

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -191,7 +191,7 @@ TEST(correctParams, print_pets_by_type) {
     putchar('\n');
     print_pets_by_type(pets, &pets_number, (char*) requested_pet_type.c_str());
     setbuf(stdout, NULL);
-    std::string expected_string = "name: cats_name, type: dog, color: cats_color\n";
+    std::string expected_string = "name: cats_name, type: dog, color: cats_color\n";  // NOLINT
     EXPECT_EQ(expected_string, file_string);
 }
 
